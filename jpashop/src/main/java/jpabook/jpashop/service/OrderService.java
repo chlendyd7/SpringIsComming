5b369a257676dbf5098e8a5867bb51a353887f3a
Doo Young <chlendyd7@naver.com>
2025-09-25 09:31:24 +0900

    typo: memeber -> member

--- jpashop/src/main/java/jpabook/jpashop/service/OrderService.java
+++ jpashop/src/main/java/jpabook/jpashop/service/OrderService.java
@@ -26,20 +26,20 @@ public class OrderService {
      * 주문
      */
     @Transactional
-    public Long order(Long memeberId, Long itemId, int count) {
+    public Long order(Long memberId, Long itemId, int count) {
 
         //엔티티 조회
-        Member memeber = memberRepository.findOne(memeberId);
+        Member member = memberRepository.findOne(memberId);
         Item item = itemRepository.findOne(itemId);
 
         Delivery delivery = new Delivery();
-        delivery.setAddress(memeber.getAddress());
+        delivery.setAddress(member.getAddress());
 
         //주문상품 생성
         OrderItem orderItem = OrderItem.createOrderItem(item, item.getPrice(), count);
 
         //주문 생성
-        Order order = Order.createOrder(memeber, delivery, orderItem);
+        Order order = Order.createOrder(member, delivery, orderItem);
 
         //주문 저장
         orderRepository.save(order);
